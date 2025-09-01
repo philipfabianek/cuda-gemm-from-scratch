95ccb2ce3f52b62ae17c09aaf639c8b1c6a056da
Philip Fabianek <fabianekphilip@gmail.com>
2025-09-01 16:29:58 +0200

    Refactor indexing

--- src/kernels/05_transposed.cuh
+++ src/kernels/05_transposed.cuh
@@ -14,9 +14,9 @@ __global__ void sgemm_transposed_kernel(int M, int N, int K, float alpha,
   __shared__ float B_tile[BK * BN];
 
   // One thread loads chunks of 4 elements into smem
-  const int A_smem_col = threadIdx.x % (BK / 4);
+  const int A_smem_col = (threadIdx.x % (BK / 4)) * 4;
   const int A_smem_row = threadIdx.x / (BK / 4);
-  const int B_smem_col = threadIdx.x % (BN / 4);
+  const int B_smem_col = (threadIdx.x % (BN / 4)) * 4;
   const int B_smem_row = threadIdx.x / (BN / 4);
 
   // Multiply by 4 because of the adjusted loading
@@ -43,20 +43,20 @@ __global__ void sgemm_transposed_kernel(int M, int N, int K, float alpha,
     for (int A_row_offset = 0; A_row_offset < BM;
          A_row_offset += A_row_stride) {
       float4 tmp = reinterpret_cast<float4 *>(
-          &A[(A_smem_row + A_row_offset) * K + A_smem_col * 4])[0];
-      A_tile[(A_smem_col * 4 + 0) * BM + (A_smem_row + A_row_offset)] = tmp.x;
-      A_tile[(A_smem_col * 4 + 1) * BM + (A_smem_row + A_row_offset)] = tmp.y;
-      A_tile[(A_smem_col * 4 + 2) * BM + (A_smem_row + A_row_offset)] = tmp.z;
-      A_tile[(A_smem_col * 4 + 3) * BM + (A_smem_row + A_row_offset)] = tmp.w;
+          &A[(A_smem_row + A_row_offset) * K + A_smem_col])[0];
+      A_tile[(A_smem_col + 0) * BM + (A_smem_row + A_row_offset)] = tmp.x;
+      A_tile[(A_smem_col + 1) * BM + (A_smem_row + A_row_offset)] = tmp.y;
+      A_tile[(A_smem_col + 2) * BM + (A_smem_row + A_row_offset)] = tmp.z;
+      A_tile[(A_smem_col + 3) * BM + (A_smem_row + A_row_offset)] = tmp.w;
     }
 
     // Load values from B into smem using a vectorized load
     for (int B_row_offset = 0; B_row_offset < BK;
          B_row_offset += B_row_stride) {
       reinterpret_cast<float4 *>(
-          &B_tile[(B_smem_row + B_row_offset) * BN + B_smem_col * 4])[0] =
+          &B_tile[(B_smem_row + B_row_offset) * BN + B_smem_col])[0] =
           reinterpret_cast<float4 *>(
-              &B[(B_smem_row + B_row_offset) * N + B_smem_col * 4])[0];
+              &B[(B_smem_row + B_row_offset) * N + B_smem_col])[0];
     }
 
     // Synchronize so that all tile values are loaded
@@ -70,22 +70,20 @@ __global__ void sgemm_transposed_kernel(int M, int N, int K, float alpha,
     for (int k = 0; k < BK; ++k) {
       // Load <TM> consecutive elements within one row (horizontal slice) from
       // the A tile and store them in registers
-      for (int i = 0; i < (TM / 4); i++) {
+      for (int i = 0; i < TM; i += 4) {
         // This explicit vectorization does not improve performance
         // which means the loads were already vectorized
-        reinterpret_cast<float4 *>(&A_reg[i * 4])[0] =
-            reinterpret_cast<float4 *>(
-                &A_tile[k * BM + C_inner_row + i * 4])[0];
+        reinterpret_cast<float4 *>(&A_reg[i])[0] =
+            reinterpret_cast<float4 *>(&A_tile[k * BM + C_inner_row + i])[0];
       }
 
       // Load <TN> consecutive elements within one row (horizontal slice) from
       // the B tile and store them in registers
-      for (int j = 0; j < (TN / 4); j++) {
+      for (int j = 0; j < TN; j += 4) {
         // This explicit vectorization does not improve performance
         // which means the loads were already vectorized
-        reinterpret_cast<float4 *>(&B_reg[j * 4])[0] =
-            reinterpret_cast<float4 *>(
-                &B_tile[k * BN + C_inner_col + j * 4])[0];
+        reinterpret_cast<float4 *>(&B_reg[j])[0] =
+            reinterpret_cast<float4 *>(&B_tile[k * BN + C_inner_col + j])[0];
       }
 
       // Compute products of all pairs from the respective slices
@@ -103,16 +101,16 @@ __global__ void sgemm_transposed_kernel(int M, int N, int K, float alpha,
 
   // Each thread saves a minitile of size TM x TN
   for (int i = 0; i < TM; i++) {
-    for (int j = 0; j < (TN / 4); j++) {
+    for (int j = 0; j < TN; j += 4) {
       // Use vectorization for global memory writes
       float4 tmp = reinterpret_cast<float4 *>(
-          &C[(C_inner_row + i) * N + (C_inner_col + j * 4)])[0];
-      tmp.x = alpha * sums[i * TN + j * 4 + 0] + beta * tmp.x;
-      tmp.y = alpha * sums[i * TN + j * 4 + 1] + beta * tmp.y;
-      tmp.z = alpha * sums[i * TN + j * 4 + 2] + beta * tmp.z;
-      tmp.w = alpha * sums[i * TN + j * 4 + 3] + beta * tmp.w;
+          &C[(C_inner_row + i) * N + (C_inner_col + j)])[0];
+      tmp.x = alpha * sums[i * TN + j + 0] + beta * tmp.x;
+      tmp.y = alpha * sums[i * TN + j + 1] + beta * tmp.y;
+      tmp.z = alpha * sums[i * TN + j + 2] + beta * tmp.z;
+      tmp.w = alpha * sums[i * TN + j + 3] + beta * tmp.w;
       reinterpret_cast<float4 *>(
-          &C[(C_inner_row + i) * N + (C_inner_col + j * 4)])[0] = tmp;
+          &C[(C_inner_row + i) * N + (C_inner_col + j)])[0] = tmp;
     }
   }
 }
