dde9b6203cac23e0ad1ab7b320b8c2dd7c42887c
Philip Fabianek <fabianekphilip@gmail.com>
2025-10-03 19:23:22 +0200

    Refactor code

--- src/kernels/11_memory_swizzling.cuh
+++ src/kernels/11_memory_swizzling.cuh
@@ -50,48 +50,54 @@ __global__ void memory_swizzling_kernel(int M, int N, int K, float alpha,
   __shared__ half a_shmem[BM * BK];
   __shared__ half b_shmem[BK * BN];
 
-  constexpr unsigned int a_shmem_row_stride = (num_threads * 8) / BK;
-  constexpr unsigned int a_shmem_iters = BM / a_shmem_row_stride;
+  constexpr int a_shmem_row_stride = (num_threads * 8) / BK;
+  constexpr int a_shmem_iters = BM / a_shmem_row_stride;
 
-  constexpr unsigned int b_shmem_row_stride = (num_threads * 8) / BN;
-  constexpr unsigned int b_shmem_iters = BK / b_shmem_row_stride;
+  constexpr int b_shmem_row_stride = (num_threads * 8) / BN;
+  constexpr int b_shmem_iters = BK / b_shmem_row_stride;
 
   // Swizzling parameters, mask is shifted by 3 bits because of the vectorization
   // and then by a number of bits needed for column index representation,
   // the _bytes masks are used for ldmatrix instructions because they use byte offsets
   // instead of element offsets, half is 2 bytes hence the << 1 shift
-  constexpr unsigned int a_swizzle_bits = int_log2(BK / 8);
-  constexpr unsigned int a_swizzle_mask = 0b111 << (3 + a_swizzle_bits);
-  constexpr unsigned int a_swizzle_mask_bytes = a_swizzle_mask << 1;
+  constexpr int a_swizzle_bits = int_log2(BK / 8);
+  constexpr int a_swizzle_mask = 0b111 << (3 + a_swizzle_bits);
+  constexpr int a_swizzle_mask_bytes = a_swizzle_mask << 1;
 
-  constexpr unsigned int b_swizzle_bits = int_log2(BN / 8);
-  constexpr unsigned int b_swizzle_mask = 0b111 << (3 + b_swizzle_bits);
-  constexpr unsigned int b_swizzle_mask_bytes = b_swizzle_mask << 1;
+  constexpr int b_swizzle_bits = int_log2(BN / 8);
+  constexpr int b_swizzle_mask = 0b111 << (3 + b_swizzle_bits);
+  constexpr int b_swizzle_mask_bytes = b_swizzle_mask << 1;
 
   for (int block_k = 0; block_k < K; block_k += BK) {
-    unsigned int a_shmem_row = (threadIdx.x * 8) / BK;
-    const unsigned int a_shmem_col = (threadIdx.x * 8) % BK;
+    int a_shmem_row = (threadIdx.x * 8) / BK;
+    const int a_shmem_col = (threadIdx.x * 8) % BK;
 
     // Load swizzled A tile into shared memory
-    for (unsigned int i = 0; i < a_shmem_iters; i++) {
-      const unsigned src_idx = a_shmem_row * K + (block_k + a_shmem_col);
-      unsigned dst_idx = a_shmem_row * BK + a_shmem_col;
-      dst_idx = dst_idx ^ ((dst_idx & a_swizzle_mask) >> a_swizzle_bits);
-      reinterpret_cast<float4 *>(&a_shmem[dst_idx])[0] =
-          reinterpret_cast<const float4 *>(&d_A[src_idx])[0];
+    for (int i = 0; i < a_shmem_iters; i++) {
+      const int gmem_idx = a_shmem_row * K + (block_k + a_shmem_col);
+      int smem_idx = a_shmem_row * BK + a_shmem_col;
+
+      // Swizzle
+      smem_idx = smem_idx ^ ((smem_idx & a_swizzle_mask) >> a_swizzle_bits);
+
+      reinterpret_cast<float4 *>(&a_shmem[smem_idx])[0] =
+          reinterpret_cast<const float4 *>(&d_A[gmem_idx])[0];
       a_shmem_row += a_shmem_row_stride;
     }
 
-    unsigned int b_shmem_row = (threadIdx.x * 8) / BN;
-    const unsigned int b_shmem_col = (threadIdx.x * 8) % BN;
+    int b_shmem_row = (threadIdx.x * 8) / BN;
+    const int b_shmem_col = (threadIdx.x * 8) % BN;
 
     // Load swizzled B tile into shared memory
-    for (unsigned int i = 0; i < b_shmem_iters; i++) {
-      const unsigned src_idx = (block_k + b_shmem_row) * N + b_shmem_col;
-      unsigned dst_idx = b_shmem_row * BN + b_shmem_col;
-      dst_idx = dst_idx ^ ((dst_idx & b_swizzle_mask) >> b_swizzle_bits);
-      reinterpret_cast<float4 *>(&b_shmem[dst_idx])[0] =
-          reinterpret_cast<const float4 *>(&d_B[src_idx])[0];
+    for (int i = 0; i < b_shmem_iters; i++) {
+      const int gmem_idx = (block_k + b_shmem_row) * N + b_shmem_col;
+      int smem_idx = b_shmem_row * BN + b_shmem_col;
+
+      // Swizzle
+      smem_idx = smem_idx ^ ((smem_idx & b_swizzle_mask) >> b_swizzle_bits);
+
+      reinterpret_cast<float4 *>(&b_shmem[smem_idx])[0] =
+          reinterpret_cast<const float4 *>(&d_B[gmem_idx])[0];
       b_shmem_row += b_shmem_row_stride;
     }
 
@@ -99,22 +105,20 @@ __global__ void memory_swizzling_kernel(int M, int N, int K, float alpha,
 
     // This loop level only affects register usage
     for (int warp_k = 0; warp_k < BK; warp_k += WK) {
-      half *A_warp_tile = &a_shmem[warptile_row * BK + warp_k];
-      uint32_t A_warp_tile_byte_offset = cvta_to_shared_u32(A_warp_tile);
+      uint32_t a_shmem_byte_offset =
+          cvta_to_shared_u32(&a_shmem[warptile_row * BK + warp_k]);
 
       // Load values from shared memory A tile to registers
       for (int m_tile = 0; m_tile < TILES_M; m_tile++) {
         for (int k_frag = 0; k_frag < TILES_K; k_frag++) {
-          const unsigned int mma_tile_byte_offset =
-              ((m_tile * MMA_M * BK) + (k_frag * MMA_K)) * sizeof(half);
-          const unsigned int thread_byte_offset =
-              (threadIdx.x % MMA_M) * BK * sizeof(half);
-          unsigned int thread_offset_bytes = A_warp_tile_byte_offset +
-                                             mma_tile_byte_offset +
-                                             thread_byte_offset;
-          thread_offset_bytes =
-              thread_offset_bytes ^
-              ((thread_offset_bytes & a_swizzle_mask_bytes) >> a_swizzle_bits);
+          const int thread_byte_offset =
+              ((m_tile * MMA_M + lane_id) * BK + k_frag * MMA_K) * sizeof(half);
+          int total_byte_offset = a_shmem_byte_offset + thread_byte_offset;
+
+          // Swizzle
+          total_byte_offset =
+              total_byte_offset ^
+              ((total_byte_offset & a_swizzle_mask_bytes) >> a_swizzle_bits);
 
           // Providing shared memory offset is actually a little faster
           // than not using the .shared modifier and providing a generic pointer,
@@ -122,26 +126,25 @@ __global__ void memory_swizzling_kernel(int M, int N, int K, float alpha,
           asm volatile(
               "ldmatrix.sync.aligned.m8n8.x2.shared.b16 {%0, %1}, [%2];"
               : "=r"(a_regs[m_tile][k_frag][0]), "=r"(a_regs[m_tile][k_frag][1])
-              : "r"(thread_offset_bytes));
+              : "r"(total_byte_offset));
         }
       }
 
-      half *B_warp_tile = &b_shmem[warp_k * BN + warptile_col];
-      uint32_t B_warp_tile_byte_offset = cvta_to_shared_u32(B_warp_tile);
+      uint32_t b_shmem_byte_offset =
+          cvta_to_shared_u32(&b_shmem[warp_k * BN + warptile_col]);
 
       // Load values from shared memory B tile to registers
       for (int k_frag = 0; k_frag < TILES_K; k_frag++) {
         for (int n_tile = 0; n_tile < TILES_N; n_tile++) {
-          const unsigned int mma_tile_byte_offset =
-              ((k_frag * MMA_K * BN) + (n_tile * MMA_N)) * sizeof(half);
-          const unsigned int thread_byte_offset =
-              (threadIdx.x % MMA_K) * BN * sizeof(half);
-          unsigned int thread_offset_bytes = B_warp_tile_byte_offset +
-                                             mma_tile_byte_offset +
-                                             thread_byte_offset;
-          thread_offset_bytes =
-              thread_offset_bytes ^
-              ((thread_offset_bytes & b_swizzle_mask_bytes) >> b_swizzle_bits);
+          const int thread_byte_offset =
+              ((k_frag * MMA_K + threadIdx.x % MMA_K) * BN + n_tile * MMA_N) *
+              sizeof(half);
+          int total_byte_offset = b_shmem_byte_offset + thread_byte_offset;
+
+          // Swizzle
+          total_byte_offset =
+              total_byte_offset ^
+              ((total_byte_offset & b_swizzle_mask_bytes) >> b_swizzle_bits);
 
           // Providing shared memory offset is actually a little faster
           // than not using the .shared modifier and providing a generic pointer,
@@ -149,7 +152,7 @@ __global__ void memory_swizzling_kernel(int M, int N, int K, float alpha,
           asm volatile(
               "ldmatrix.sync.aligned.m8n8.x1.trans.shared.b16 {%0}, [%1];"
               : "=r"(b_regs[k_frag][n_tile][0])
-              : "r"(thread_offset_bytes));
+              : "r"(total_byte_offset));
         }
       }
 
